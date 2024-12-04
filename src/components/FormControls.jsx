eec77bd60406cb7cc4f012cfe7accd037cb079a3
sevro49 <95761902+sevro49@users.noreply.github.com>
2024-12-04 23:42:01 +0300

    feat: added stage2 ui

--- src/components/FormControls.jsx
+++ src/components/FormControls.jsx
@@ -4,11 +4,15 @@ import React from 'react';
 import formControls from './FormControls.module.scss';
 import { useSelector } from 'react-redux'; 
 import Button from '@mui/material/Button';
-import { useRouter } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
+import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
 
 const FormControls = () => {
   const router = useRouter();  // navigate with useRouter
   const { isSection1Valid } = useSelector((state) => state.interview);
+
+  const pathname = usePathname();
+  const currentStep = pathname.split('/').pop();
 
   const handleProceed = () => {
     if (isSection1Valid) {
@@ -17,20 +21,24 @@ const FormControls = () => {
       alert('Please fill all the fields to proceed');
     }
   };
+  const handlePrevious = () => {
+    router.push('/interview-creation/step1');
+  };
 
-  console.log(isSection1Valid)
+  // console.log(isSection1Valid)
 
   return (
     <section className={formControls.pageContainer}>
       <div className={formControls.btnWrapper}>
-        <Button className={formControls.draft}>DRAFT</Button>
-        <Button 
-          className={formControls.proceed} 
-          onClick={handleProceed}
+        <div className={formControls.previousWrapper}>
+          <Button onClick={handlePrevious} className={`${formControls.previous} ${currentStep === "step1" ? formControls.hidePrevious : ""}`} ><KeyboardArrowLeftIcon className={handlePrevious.icon} /> PREVIOUS</Button>
+        </div>
+        <div className={formControls.proceedWrapper}>
+          <Button className={formControls.draft}>DRAFT</Button>
+          <Button className={formControls.proceed} onClick={handleProceed}
           disabled={!isSection1Valid}  // isSection1Valid === false => disabled 
-        >
-          PROCEED
-        </Button>
+          >PROCEED</Button>
+        </div>
       </div>
     </section>
   );
