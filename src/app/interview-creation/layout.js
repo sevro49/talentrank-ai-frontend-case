d4b2270f35a42c6caad6e39ae93afd7af3db0412
sevro49 <95761902+sevro49@users.noreply.github.com>
2024-12-05 23:12:02 +0300

    refactor: suspanse fix 4

--- src/app/interview-creation/layout.js
+++ src/app/interview-creation/layout.js
@@ -1,12 +1,22 @@
+"use client";
+
+import React, { Suspense } from "react";
 import FormControls from "../../components/FormControls";
 import Header from "../../components/Header";
 
+// Yüklenme sırasında gösterilecek içerik
+const LoadingFallback = () => {
+  return <div style={{ textAlign: "center", padding: "20px" }}>Loading content...</div>;
+};
+
 export default function InterviewCreationLayout({ children }) {
   return (
-    <div>
-      <Header />
-      {children}
-      <FormControls />
-    </div>
+    <Suspense fallback={<LoadingFallback />}>
+      <div>
+        <Header />
+        <main style={{ padding: "20px" }}>{children}</main>
+        <FormControls />
+      </div>
+    </Suspense>
   );
 }
